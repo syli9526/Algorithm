f306533349b6cb85a54f73a021c3284f7584a241
syli9526 <syli5806@gmail.com>
2019-03-11 16:31:11 +0900

    동적프로그래밍

---
+++ DynamicProgramming/BJ2294.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+
+const int MAX = 100001;
+int N, K, dp[MAX];
+
+int main() {
+
+    scanf("%d %d", &N, &K);
+
+    fill(dp, dp + K + 1, 100000);
+
+    for (int i = 0, x; i < N; ++i) scanf("%d", &x), dp[x] = 1;
+
+    for (int i = 1; i <= K; i++)
+        for (int j = i - 1; j > 0; j--)
+            dp[i] = min(dp[i], dp[j] + dp[i - j]);
+    printf("%d", dp[K] < 100000 ? dp[K] : -1);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
